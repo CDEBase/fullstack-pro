604dd6559fff9bbd855dc60e9655d67b04befcab
veeramarni <mvasireddy@cdmbase.com>
2017-05-08 08:46:04 -0400

    updates

--- packages/sample-browser-server/src/index.tsx
+++ packages/sample-browser-server/src/index.tsx
@@ -3,7 +3,7 @@ import * as ReactDOM from 'react-dom'
 import * as redux from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
-
+import { ApolloClient, createNetworkInterface, ApolloProvider } from 'react-apollo';
 import {
   reducers,
   Store,
@@ -12,8 +12,19 @@ import {
 import { Counter } from './components/counter'
 import './index.css';
 
-let store: redux.Store<Store.All> = redux.createStore(
-  reducers,
+const networkInterface = createNetworkInterface({
+  uri: 'http://localhost:3000/graphql'
+});
+
+const client = new ApolloClient({
+  networkInterface: networkInterface
+});
+
+let store: redux.Store<any> = redux.createStore(
+  redux.combineReducers({
+    reducers,
+    apollo: client.reducer(),
+  }),
   {} as Store.All,
   redux.applyMiddleware(thunk),
 )
@@ -22,8 +33,8 @@ let store: redux.Store<Store.All> = redux.createStore(
 window.addEventListener('DOMContentLoaded', () => {
   const rootEl = document.getElementById('redux-app-root')
   if (rootEl) ReactDOM.render(
-    <Provider store={store}>
+    <ApolloProvider store={store} client={client}>
       <Counter label='count:' />
-    </Provider>
+    </ApolloProvider>
   , rootEl)
 })
