ca06fa776f5b78edcf52c612a32121597f5a24fa
veeramarni <mvasireddy@cdmbase.com>
2017-10-14 18:28:28 -0400

    update frontend port

--- servers/frontend-server/app.js
+++ servers/frontend-server/app.js
@@ -9,6 +9,7 @@ app.get('/*', function (req, res) {
     res.sendfile("index.html", { root: path.join(__dirname, '/dist') });
 });
 
-app.listen(3000, function () {
-    console.log("Frontend App is running at localhost: 3000")
+const port = process.env.GRAPHQL_CLIENT_PORT || 3000
+app.listen(port, function () {
+    console.log(`Frontend App is running at localhost: ${port}`)
 });
