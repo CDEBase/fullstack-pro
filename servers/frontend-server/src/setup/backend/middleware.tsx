475a606c4f94756fc6b1b72bff6d2d6ac04bf45b
veeramarni <mvasireddy@cdmbase.com>
2017-10-24 14:25:49 -0400

    fixes #18 close

--- servers/frontend-server/src/setup/backend/middleware.tsx
+++ servers/frontend-server/src/setup/backend/middleware.tsx
@@ -11,7 +11,7 @@ import * as path from 'path';
 import * as fs from 'fs';
 const { renderToMarkup } = require('fela-dom');
 import { Provider as ReduxProvider } from 'react-redux';
-import { logger } from '@sample-stack/client-core';
+import { logger } from '@sample-stack/utils';
 import { createApolloClient } from '../apollo-client';
 import * as ReactFela from 'react-fela';
 import createRenderer from './felaRenderer';
@@ -23,50 +23,56 @@ import publicEnv from '../../config/public-config';
 
 let assetMap;
 async function renderServerSide(req, res) {
+    try {
+        const client = createApolloClient();
 
-    const client = createApolloClient();
+        let initialState = {};
+        const store = createReduxStore(initialState, client);
+        const renderer = createRenderer();
+        const component = (
+            <ApolloProvider store={store} client={client}>
+                <ReduxProvider store={store} >
+                    <ReactFela.Provider renderer={renderer} >
+                        <Component />
+                    </ReactFela.Provider>
+                </ReduxProvider>
+            </ApolloProvider>
+        );
 
-    let initialState = {};
-    const store = createReduxStore(initialState, client);
-    const renderer = createRenderer();    
-    const component = (
-        <ApolloProvider store={store} client={client}>
-            <ReduxProvider store={store} >
-                <ReactFela.Provider renderer={renderer} >
-                    <Component />
-                </ReactFela.Provider>
-            </ReduxProvider>
-        </ApolloProvider>
-    );
+        const appCss = renderToMarkup(renderer);
+        await getDataFromTree(component);
+        res.status(200);
 
-    const appCss = renderToMarkup(renderer);    
-    await getDataFromTree(component);
-    res.status(200);
-    const html = ReactDOMServer.renderToString(component);
-    const helmet = Helmet.renderStatic(); // Avoid memory leak while tracking mounted instances
 
-    if (__DEV__ || !assetMap) {
-        assetMap = JSON.parse(fs.readFileSync(path.join(SETTINGS.frontendBuildDir, 'web', 'assets.json')).toString());
+        const html = ReactDOMServer.renderToString(component);
+
+
+        const helmet = Helmet.renderStatic(); // Avoid memory leak while tracking mounted instances
+
+        if (__DEV__ || !assetMap) {
+            assetMap = JSON.parse(fs.readFileSync(path.join(SETTINGS.frontendBuildDir, 'web', 'assets.json')).toString());
+        }
+        const apolloState = Object.assign({}, client.store.getState());
+        const env = {
+            ...publicEnv
+        };
+
+        const page = (
+            <Html
+                content={html}
+                state={apolloState}
+                assetMap={assetMap}
+                helmet={helmet}
+                css={appCss}
+                env={env}
+            />
+        );
+        res.send(`<!doctype html>\n${ReactDOMServer.renderToStaticMarkup(page)}`);
+        res.end();
+    } catch (err) {
+        logger.error('SERVER SIDE RENDER failed due to (%j) ', err);
     }
-    const apolloState = Object.assign({}, client.store.getState());
-    const env = {
-        ...publicEnv
-    };
-
-    const page = (
-        <Html
-            content={html}
-            state={apolloState}
-            assetMap={assetMap}
-            helmet={helmet}
-            css={appCss}
-            env={env}
-        />
-    );
-    res.send(`<!doctype html>\n${ReactDOMServer.renderToStaticMarkup(page)}`);
-    res.end();
 }
-
 export const websiteMiddleware = async (req, res, next) => {
     try {
         if (req.url.indexOf('.') < 0 && __SSR__) {
