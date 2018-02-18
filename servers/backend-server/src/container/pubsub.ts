e5778b98ddd2a6a5b318e83102d9b80805c142f9
veeramarni <mvasireddy@cdmbase.com>
2018-02-17 19:51:42 -0500

    fix nats issue

--- servers/backend-server/src/container/pubsub.ts
+++ servers/backend-server/src/container/pubsub.ts
@@ -3,6 +3,14 @@ import { NatsPubSub } from 'graphql-nats-subscriptions';
 import { addApolloLogging } from 'apollo-logger';
 import { logger } from '@sample-stack/utils';
 import { SETTINGS } from '../config';
+import * as nats from 'nats';
+
+export const client = nats.connect({
+    'url': SETTINGS.NATS_URL,
+    'user': SETTINGS.NATS_USER,
+    'pass': SETTINGS.NATS_PW as string,
+    reconnectTimeWait: 1000,
+});
 
 export const pubsub = process.env.NODE_ENV === 'development' ?
-    SETTINGS.apolloLogging ? addApolloLogging(new PubSub()) : new PubSub() : new NatsPubSub({ logger });
+    SETTINGS.apolloLogging ? addApolloLogging(new PubSub()) : new PubSub() : new NatsPubSub({ client, logger });
