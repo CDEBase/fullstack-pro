77c1f3e90f91a5ab83108d49d56c0e88e9a67790
veeramarni <mvasireddy@cdmbase.com>
2018-06-15 14:04:20 -0400

    udpate

--- servers/frontend-server/src/backend/website.tsx
+++ servers/frontend-server/src/backend/website.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as ReactDOMServer from 'react-dom/server';
 import { ApolloProvider, getDataFromTree } from 'react-apollo';
-import { addApolloLogging } from 'apollo-logger';
+// import { addApolloLogging } from 'apollo-logger';
 import { addPersistedQueries } from 'persistgraphql';
 import { Html } from './ssr/html';
 import { Component } from '../components';
