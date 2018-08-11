21020af74217d43f74b1556cd5cb7a4ed6942d30
veeramarni <mvasireddy@cdmbase.com>
2018-08-11 12:16:20 -0400

    seperate nats configuration

--- servers/backend-server/src/modules/module.ts
+++ servers/backend-server/src/modules/module.ts
@@ -1,11 +1,12 @@
 import { ContainerModule, interfaces } from 'inversify';
 import { Feature, logger } from '@common-stack/server-core';
 import CounterModule from '@sample-stack/counter/lib/server';
-import { pubsub, client } from './pubsub';
+import { pubsub } from './pubsub';
 import { generateMongo } from '@common-stack/store-mongo';
 import * as NATS from 'nats';
 import * as Hemera from 'nats-hemera';
 import { config } from '../config';
+import { hemeraGen } from './nats-connection';
 
 export const settings = {
     mongoConnection: generateMongo(config.MONGO_URL),
@@ -23,13 +24,7 @@ const defaultModule =
         bind('MongoOptions').toConstantValue({});
 
         if (process.env.NODE_ENV !== 'development') {
-            const hemera = new Hemera(client(), {
-                logLevel: config.HEMERA_LOG_LEVEL as Hemera.LogLevel || 'info',
-                childLogger: true,
-                tag: 'hemera-server',
-                timeout: 10000,
-            });
-            bind('Hemera').toConstantValue(hemera);
+            bind('Hemera').toConstantValue(hemeraGen());
         }
     });
 
--- servers/backend-server/src/modules/pubsub.ts
+++ servers/backend-server/src/modules/pubsub.ts
@@ -3,15 +3,8 @@ import { NatsPubSub } from 'graphql-nats-subscriptions';
 import { wrapPubSub } from 'apollo-logger';
 import { logger } from '@common-stack/server-core';
 import { config } from '../config';
-import * as nats from 'nats';
-
-export const client: () => nats.Client = () => nats.connect({
-    'url': config.NATS_URL,
-    'user': config.NATS_USER,
-    'pass': config.NATS_PW as string,
-    reconnectTimeWait: 1000,
-});
+import { clientGen } from './nats-connection';
 
 export const pubsub = process.env.NODE_ENV === 'development' ?
     config.apolloLogging ? wrapPubSub(new PubSub(), { logger: logger.debug.bind(logger) }) :
-        new PubSub() : new NatsPubSub({ client: client(), logger });
+        new PubSub() : new NatsPubSub({ client: clientGen(), logger });
