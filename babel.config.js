157dfebf9a215ee0591ce5b0fb2f24c8ec674276
veeramarni <marni.srikanth@gmail.com>
2019-02-28 09:15:01 -0500

    added babel

---
+++ babel.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    compact: false,
+    presets: ['@babel/preset-typescript', '@babel/preset-react', ['@babel/preset-env', { modules: 'commonjs' }]],
+    plugins: [
+        '@babel/plugin-transform-modules-commonjs',
+        '@babel/plugin-transform-destructuring',
+        '@babel/plugin-transform-regenerator',
+        '@babel/plugin-transform-runtime',
+        "@babel/plugin-syntax-dynamic-import",
+        '@babel/plugin-proposal-class-properties',
+        ['@babel/plugin-proposal-decorators', { legacy: true }],
+        '@babel/plugin-proposal-object-rest-spread',
+        //   ['styled-components', { ssr: true }],
+        //   ['import', { libraryName: 'antd-mobile' }]
+    ],
+    env: {
+        production: {
+            compact: true
+        }
+    }
+};
