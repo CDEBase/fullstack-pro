cce19cb8e392183131fbb4a3a699a1afb3a91110
veeramarni <marni.srikanth@gmail.com>
2019-05-06 18:04:11 -0400

    update apollo-client.test

--- packages-modules/counter/browser/src/apollo-server-n-client/graphql/__tests__/apollo-client-test-helper.ts
+++ packages-modules/counter/browser/src/apollo-server-n-client/graphql/__tests__/apollo-client-test-helper.ts
@@ -3,9 +3,9 @@ import { SchemaLink } from 'apollo-link-schema';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { withClientState } from 'apollo-link-state';
 import { ApolloLink } from 'apollo-link';
-import { makeExecutableSchema, addMockFunctionsToSchema } from 'graphql-tools';
-import * as schema from '../schema/schema.graphql';
+// import * as schema from '../schema/schema.graphql';
 import { resolvers, defaults } from '../resolvers';
+import { dataIdFromObject } from '../id-generation';
 
 const defaultSchema = `
 type Query {
@@ -16,25 +16,37 @@ type Mutation {
 }
 `;
 
-const cache = new InMemoryCache();
+const cache = new InMemoryCache({
+    dataIdFromObject: (object) => getDataIdFromObject(object),
+});
 
 const linkState = withClientState({
     cache,
     resolvers,
     defaults,
-    // typeDefs: defaultSchema.concat(schema),
+    // typeDefs: defaultSchema.concat(schema as any), // if client schema exist
+
 });
 const links = [linkState];
 
 const client = new ApolloClient({
     queryDeduplication: true,
-    // dataIdFromObject: (result) => modules.getDataIdFromObject(result),
     link: ApolloLink.from(links),
     cache,
 });
 
+
+function getDataIdFromObject(result: any) {
+    if (dataIdFromObject[result.__typename]) {
+        return dataIdFromObject[result.__typename](result);
+    }
+    return result.id || result._id;
+}
+
+
 export {
     client,
+    getDataIdFromObject,
 };
 
 
