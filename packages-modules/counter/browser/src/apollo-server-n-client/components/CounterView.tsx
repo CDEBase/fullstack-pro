f3831e01c35c08314c7b97c0a8f2bc439bbd6051
veeramarni <marni.srikanth@gmail.com>
2019-10-09 17:21:59 -0400

    update changes

--- packages-modules/counter/browser/src/apollo-server-n-client/components/CounterView.tsx
+++ packages-modules/counter/browser/src/apollo-server-n-client/components/CounterView.tsx
@@ -1,18 +1,20 @@
 import * as React from 'react';
-import Helmet from 'react-helmet';
+import { Helmet } from 'react-helmet';
 
-
-const CounterView = ({ loading, counter, addCounter, reduxCount, onReduxIncrement, counterState, addCounterState }: any) => {
+const CounterView = ({
+  loading,
+  counter,
+  addCounter,
+  reduxCount,
+  onReduxIncrement,
+  counterState,
+  addCounterState,
+}: any) => {
   const renderMetaData = () => (
-    <Helmet
-      title={`Counter`}
-      meta={[
-        {
-          name: 'description',
-          content: `Counter example page`,
-        },
-      ]}
-    />
+    <Helmet>
+      <title>Counter</title>
+      <meta name="description" content="Counter example page" />
+    </Helmet>
   );
 
   if (loading) {
@@ -28,24 +30,37 @@ const CounterView = ({ loading, counter, addCounter, reduxCount, onReduxIncremen
         {renderMetaData()}
         <section>
           <p>
-            Current counter, is {counter.amount}. This is being stored server-side in the database and using Apollo
-            subscription for real-time updates.
+            Current counter, is {counter.amount}. This is being stored
+            server-side in the database and using Apollo subscription for
+            real-time updates.
           </p>
           <button id="graphql-button" color="primary" onClick={addCounter(1)}>
             Click to increase counter
           </button>
         </section>
         <section>
-          <p>Current reduxCount, is {reduxCount}. This is being stored client-side with Redux.</p>
-          <button id="redux-button" color="primary" onClick={onReduxIncrement(1)}>
+          <p>
+            Current reduxCount, is {reduxCount}. This is being stored
+            client-side with Redux.
+          </p>
+          <button
+            id="redux-button"
+            color="primary"
+            onClick={onReduxIncrement(1)}
+          >
             Click to increase reduxCount
           </button>
         </section>
         <section>
           <p>
-            Current apolloLinkStateCount, is {counterState}. This is being stored client-side with Apollo Link State.
+            Current apolloLinkStateCount, is {counterState}. This is being
+            stored client-side with Apollo Link State.
           </p>
-          <button id="apollo-link-button" color="primary" onClick={addCounterState(1)}>
+          <button
+            id="apollo-link-button"
+            color="primary"
+            onClick={addCounterState(1)}
+          >
             Click to increase apolloLinkState
           </button>
         </section>
