34bbef1d862bc996290a45534e4cb0ab04c912c5
veeramarni <marni.srikanth@gmail.com>
2019-12-03 18:18:36 -0500

    added client schema

---
+++ packages-modules/counter/browser/src/apollo-server-n-client/graphql/schema/index.ts
@@ -0,0 +1,8 @@
+
+import counterSchema from './counter-schema.graphql';
+
+const schema = `
+${counterSchema}
+`;
+export { counterSchema, schema };
+
--- packages-modules/counter/browser/src/apollo-server-n-client/module.tsx
+++ packages-modules/counter/browser/src/apollo-server-n-client/module.tsx
@@ -2,14 +2,15 @@ import * as  React from 'react';
 import { Route } from 'react-router-dom';
 
 import { reducers } from './redux';
-import { resolvers, defaults } from './graphql';
+import { resolvers, defaults, schema } from './graphql';
 
 import { Feature } from '@common-stack/client-react';
 import { filteredMenus, filteredRoutes } from './compute';
 
+console.log('---SCHEMA ', schema);
 export default new Feature({
   menuConfig: filteredMenus,
   routeConfig: filteredRoutes,
   reducer: { counter: reducers },
-  clientStateParams: { resolvers, defaults },
+  clientStateParams: { resolvers, defaults, typeDefs: schema },
 });
