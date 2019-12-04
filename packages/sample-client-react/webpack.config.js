71acf23818f49ad86cc017588ad5a73477f767e8
Igor Olshevsky <igor@adlab.pro>
2019-12-04 21:30:58 +0200

    Revert "browsers list"
    This reverts commit c60fcf11ed82f726949892344ff9f9ab24e7af4b.

--- packages/sample-client-react/webpack.config.js
+++ packages/sample-client-react/webpack.config.js
@@ -59,17 +59,7 @@ var webpack_opts = {
         fallback: 'style-loader',
         use: [
           { loader: 'css-loader', options: { importLoaders: 1 } },
-          {
-            loader: "postcss-loader",
-            options: {
-              plugins: () => [
-                require("autoprefixer")({
-                  browsers: ["> 1%", "last 2 versions"]
-                })
-              ],
-              config: { path: "./src/postcss.config.js" }
-            }
-          }
+          { loader: 'postcss-loader', options: { config: { path: './src/postcss.config.js' } } }
         ]
       })
     },
