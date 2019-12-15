5182e307e18de32a06f2a8a17392cae6a952df52
cfg111sleepy <ihorsleepy@gmail.com>
2019-12-15 18:39:55 +0200

    [+]new dep

--- tools/dependencydetect.js
+++ tools/dependencydetect.js
@@ -1,16 +1,22 @@
-const glob = require("glob");
+const glob = require('glob');
 const fs = require('fs');
+const folder = '../servers/**/';
 
-const folder = '../servers';
-
-glob(`${folder}/**/package.json`, null, (err, files) => {
+glob(`${folder}package.json`, null, (err, files) => {
     if (err) return console.log('Unable to scan directory: ' + err);
 
     files.forEach(file => {
         fs.readFile(file, 'utf-8', (err, data) => {
             if (err) return console.log('Unable to scan directory: ' + err);
-
-            console.log(JSON.parse(data));
+            const obj = JSON.parse(data);
+            const { dependencies } = obj;
+            for (let key in dependencies) {
+                if (dependencies[key].includes('file')){
+                    dependencies[key] = '^0.0.1';
+                    const str = JSON.stringify(obj, null, 2);
+                    fs.writeFileSync(file, `${str}`,  'ascii');
+                }
+            }
         });
     });
 })
