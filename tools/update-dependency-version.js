3fa5617c514007df45c6c8bd698385c8092e344c
veeramarni <marni.srikanth@gmail.com>
2019-12-16 16:35:19 -0500

    fixes #69

---
+++ tools/update-dependency-version.js
@@ -0,0 +1,41 @@
+const glob = require('glob');
+const fs = require('fs');
+const SERVER_FOLDER = './servers';
+
+glob(`${SERVER_FOLDER}/**/package.json`, null, (err, files) => {
+
+    if (err) return console.error('Unable to scan directory: ' + err);
+
+    files.forEach(file => {
+        fs.readFile(file, 'utf-8', (err, data) => {
+            if (err) return console.error('Unable to scan directory: ' + err);
+
+            const obj = JSON.parse(data);
+            const { dependencies } = obj;
+            const fileWrie = file
+
+            for (let key in dependencies) {
+                if (dependencies[key].includes('file')){
+                    const folderRoad = dependencies[key].split(':');
+                    const localFolder = folderRoad[1].slice(3);
+                    glob(`${SERVER_FOLDER}/${localFolder}/package.json`, null, (err, files) => {
+                        if (err) return console.error('Unable to scan directory: ' + err);
+
+                        files.forEach(file => {
+                            fs.readFile(file, 'utf-8', (err, data) => {
+                                if (err) return console.error('Unable to scan directory: ' + err);
+
+                                const objVersion = JSON.parse(data);
+                                const { version } = objVersion;
+                                dependencies[key] = version;
+                                const str = JSON.stringify(obj, null, 2);
+                                fs.writeFileSync(fileWrie, str,  'ascii');
+                            });
+                        });
+                    });
+                }
+            }
+        });
+    });
+})
+
