50b2084cbbdbcc8b3b9c30cfc1caa82241e4add8
Igor Olshevsky <igor@adlab.pro>
2020-01-23 17:35:11 +0200

    middleware

---
+++ servers/backend-server/src/middleware/services.ts
@@ -0,0 +1,21 @@
+import 'isomorphic-fetch';
+import modules, { serviceContext } from '../modules';
+
+let debug: boolean = false;
+if (process.env.LOG_LEVEL && process.env.LOG_LEVEL === 'trace' || process.env.LOG_LEVEL === 'debug') {
+    debug = true;
+}
+
+export const contextServicesMiddleware = (req, res, next) => {
+    Promise.all([
+        modules.createContext(req, res),
+        serviceContext(req, res),
+    ])
+        .then(([ context, services ]) => {
+            req.context = context;
+            req.services = services;
+
+            next();
+        })
+        .catch((err) => next());
+};
