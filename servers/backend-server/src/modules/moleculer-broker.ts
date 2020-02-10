bbb53443cd16afc2c76f96772616252bb30995f6
veeramarni <marni.srikanth@gmail.com>
2020-02-09 19:33:05 -0500

    update

--- servers/backend-server/src/modules/moleculer-broker.ts
+++ servers/backend-server/src/modules/moleculer-broker.ts
@@ -1,8 +1,6 @@
 import { ServiceBroker, ServiceSettingSchema } from 'moleculer';
 import * as brokerConfig from './moleculer.config';
 
-
-console.log('--BROKER CONFIG', brokerConfig)
 export const broker = new ServiceBroker({ ...brokerConfig });
 
 broker.start();
