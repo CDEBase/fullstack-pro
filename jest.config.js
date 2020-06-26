304946dd9f11096261b81210f633f75294edf69b
veeramarni <marni.srikanth@gmail.com>
2020-06-26 18:20:22 -0400

    format

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,11 @@
 const merge = require('merge')
 const baseConfig = require('./jest.config.base');
 const mongodbConfig = require('./jest.config.mongodb')
-module.exports = merge.recursive(baseConfig, mongodbConfig, {
-    globals: {
+module.exports = merge.recursive(
+    baseConfig,
+    mongodbConfig,
+    {
+        globals: {
 
-    }
-});
+        }
+    });
