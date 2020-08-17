52c52ec67ffcdd23f08644d11f08b82a0bb249a1
veeramarni <marni.srikanth@gmail.com>
2020-08-17 16:33:58 -0400

    remove moleculer module

--- packages/sample-moleculer-plugin/index.js
+++
@@ -1,9 +0,0 @@
-/*
- * samle-moleculer-plugin
- * Copyright (c) 2019 MoleculerJS (https://github.com/veeramarni/samle-moleculer-plugin)
- * MIT Licensed
- */
-
-"use strict";
-
-module.exports = require("./src");
