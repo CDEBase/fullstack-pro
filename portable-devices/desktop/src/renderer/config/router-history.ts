01dd1e078aa193bbc923dff0b9d7efefe2eb2718
Veera Marni <marni.srikanth@gmail.com>
2021-03-10 19:33:10 -0500

    adding new error bountry

--- portable-devices/desktop/src/renderer/config/router-history.ts
+++ portable-devices/desktop/src/renderer/config/router-history.ts
@@ -1,7 +1,7 @@
 import { createBrowserHistory, createMemoryHistory, createHashHistory } from 'history';
 
 if (__CLIENT__) {
-    module.exports = createHashHistory();
+    module.exports = createHashHistory(); // use hashistory in electron
 } else {
     module.exports = (url) => createMemoryHistory({
         initialEntries: [url],
