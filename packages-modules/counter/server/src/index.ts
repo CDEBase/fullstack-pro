120b2e1f34e885d20af457a9547eee8a17a16485
Veera Marni <marni.srikanth@gmail.com>
2021-04-03 20:07:25 -0400

    lint fix

--- packages-modules/counter/server/src/index.ts
+++ packages-modules/counter/server/src/index.ts
@@ -1,6 +1,6 @@
-import counter from './module';
 import { Feature } from '@common-stack/server-core';
+import counter from './module';
+
 export { CounterMockMoleculerService } from './services';
 export * from './constants';
-
 export default new Feature(counter);
--- servers/backend-server/src/modules/module.ts
+++ servers/backend-server/src/modules/module.ts
@@ -1,27 +1,23 @@
 import { ContainerModule, interfaces } from 'inversify';
 import { Feature } from '@common-stack/server-core';
 import CounterModule from '@sample-stack/counter-module-server';
-import { config } from '../config';
 import { TaggedType } from '@common-stack/core';
+import { config } from '../config';
 
 export const settings = {
-    // mongoConnection: generateMongo(config.MONGO_URL),
-    subTopic: config.CONNECTION_ID, // usually versioning
+  // mongoConnection: generateMongo(config.MONGO_URL),
+  subTopic: config.CONNECTION_ID, // usually versioning
 };
 
-
-const defaultModule =
-    () => new ContainerModule((bind: interfaces.Bind) => {
-        bind('Settings').toConstantValue(settings).whenTargetTagged('default', true);
-        bind('Settings').toConstantValue(settings).whenTargetTagged(TaggedType.MICROSERVICE, true);
-        bind('MongoOptions').toConstantValue({});
-    });
-
-
-const DefaultFeature = new Feature({
-    createContainerFunc: [defaultModule],
-    createHemeraContainerFunc: [defaultModule],
+const defaultModule = () => new ContainerModule((bind: interfaces.Bind) => {
+  bind('Settings').toConstantValue(settings).whenTargetTagged('default', true);
+  bind('Settings').toConstantValue(settings).whenTargetTagged(TaggedType.MICROSERVICE, true);
+  bind('MongoOptions').toConstantValue({});
 });
 
+const DefaultFeature = new Feature({
+  createContainerFunc: [defaultModule],
+  createHemeraContainerFunc: [defaultModule],
+});
 
 export default new Feature(DefaultFeature, CounterModule);
