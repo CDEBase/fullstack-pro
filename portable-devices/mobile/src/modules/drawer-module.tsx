0b33059a2eba8417b25d3619a4c4f8628b6acf6e
Veera Marni <marni.srikanth@gmail.com>
2021-04-06 15:46:31 -0400

    merge changes

--- portable-devices/mobile/src/modules/drawer-module.tsx
+++ portable-devices/mobile/src/modules/drawer-module.tsx
@@ -1,19 +1,14 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import * as React from 'react';
 import { History } from 'history';
 import { DrawerNavigation } from '../routes/drawer-navigation';
 
-export const DrawerRoute = (props: { history: History<any>; location: any; routes: any }) => {
-    console.log('---DRAWER ___PROPS', props);
-    return (
-        <DrawerNavigation
-            history={props.history}
-            routes={props.routes}
-            location={props.location}
-            defaultTitle="Test"
-            initialRouteName="/org"
-            // getMatchedRoute={props.getMatchedRoute}
-            screenOptions={{}}
-        />
-    );
-};
+export const DrawerRoute = (props: { history: History<any>, location: any, routes: any, getMatchedRoute: any }) => {
+    return <DrawerNavigation
+        history={props.history}
+        routes={props.routes}
+        defaultTitle={'Test'}
+        initialRouteName={'/calendar'}
+        //getMatchedRoute={props.getMatchedRoute}
+        screenOptions={{}}
+    />
+}
