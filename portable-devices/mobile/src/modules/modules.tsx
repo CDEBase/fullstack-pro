c8d2cea2c103625d83b385cecfd7c21d8834219d
Veera Marni <marni.srikanth@gmail.com>
2021-04-10 06:50:34 -0400

    changed to native router

--- portable-devices/mobile/src/modules/modules.tsx
+++ portable-devices/mobile/src/modules/modules.tsx
@@ -1,28 +1,35 @@
+/* eslint-disable react/style-prop-object */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import * as React from 'react';
+import React, { useState, useRef } from 'react';
 import { History } from 'history';
-import { Feature, FeatureWithRouterFactory, renderRoutes2 } from '@common-stack/client-react';
+import { Feature, FeatureWithRouterFactory } from '@common-stack/client-react';
+import { DrawerLayoutAndroid } from 'react-native';
+import { NativeRouter } from 'react-router-native';
+import { StatusBar } from 'expo-status-bar';
 import counterModule from '@sample-stack/counter-module-browser/lib/index.native';
 import { enableScreens } from 'react-native-screens';
 import { Navigation } from '../routes/main-navigation';
 import LayoutModule from '../components/layout/module';
+import { renderRoutes2 } from './render';
 
-const features = new Feature(FeatureWithRouterFactory, LayoutModule, counterModule);
+const features = new Feature(LayoutModule, counterModule);
 const configuredRoutes = features.getConfiguredRoutes();
 
+const routes = renderRoutes2(configuredRoutes);
 enableScreens();
 
 console.log('--ROUTES', configuredRoutes);
 export const MainRoute = (props: { history: History<any> }) => {
+    const [drawerPosition, setDrawerPosition] = useState<'left' | 'right'>('left');
+    const drawerRef = useRef(null);
+
     return (
-        <Navigation
-            history={props.history}
-            routes={configuredRoutes}
-            defaultTitle="Test"
-            initialRouteName="/"
-            screenOptions={{}}
-        />
+        <NativeRouter>
+
+                {routes}
+                <StatusBar style="auto" />
+        </NativeRouter>
     );
 };
 
---
+++ portable-devices/mobile/webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const createExpoWebpackConfigAsync = require('@expo/webpack-config');
+
+module.exports = async function (env, argv) {
+  const config = await createExpoWebpackConfigAsync(env, argv);
+
+  config.module.rules.push({
+    test: /\.js$/,
+    loader: 'babel-loader',
+    include: [path.join(__dirname, 'node_modules/react-router-native')],
+  },
+  {
+    test: /\.js$/,
+    loader: 'babel-loader',
+    include: [path.join(__dirname, '../../node_modules/react-router-native')],
+  }
+  );
+
+  return config;
+};
