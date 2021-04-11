a05d280a7ae8ed245f7c796c2996ae0ca017f6b1
Veera Marni <marni.srikanth@gmail.com>
2021-04-11 05:39:59 -0400

    update lint

--- portable-devices/desktop/webpack.renderer.additions.js
+++ portable-devices/desktop/webpack.renderer.additions.js
@@ -1,94 +1,85 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 const Dotenv = require('dotenv-webpack');
 const webpack = require('webpack');
-var dotenv = require('dotenv-safe')
+const dotenv = require('dotenv-safe');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const options = {
-    stack: [
-        "apollo",
-        "ts",
-        "react",
-        "webpack",
-        "css"
-    ],
+    stack: ['apollo', 'ts', 'react', 'webpack', 'css'],
     cache: '../../.cache',
-    backendBuildDir: "dist",
-    frontendBuildDir: "dist",
-    dllBuildDir: "dist/.build/dll",
+    backendBuildDir: 'dist',
+    frontendBuildDir: 'dist',
+    dllBuildDir: 'dist/.build/dll',
     ssr: false,
-    backendUrl: "http://localhost:8091",
+    backendUrl: 'http://localhost:8091',
     webpackDll: true,
     reactHotLoader: true,
     useDefaultPostCss: true,
     persistGraphQL: false,
     frontendRefreshOnBackendChange: true,
     nodeDebugger: false,
-    overridesConfig: "./tools/webpackAppConfig.js",
+    overridesConfig: './tools/webpackAppConfig.js',
     plugins: [
         new Dotenv({
-            path: process.env.ENV_FILE
+            path: process.env.ENV_FILE,
         }),
-        new webpack.DefinePlugin(
-            // Object.assign(
-            //     ...Object.entries(buildConfig).map(([k, v]) => ({
-            //         [k]: typeof v !== 'string' ? v : `'${v.replace(/\\/g, '\\\\')}'`
-            //     }))
-            // )
-        ),
+        new webpack.DefinePlugin(),
+        // Object.assign(
+        //     ...Object.entries(buildConfig).map(([k, v]) => ({
+        //         [k]: typeof v !== 'string' ? v : `'${v.replace(/\\/g, '\\\\')}'`
+        //     }))
+        // )
     ],
     defines: {
         __DEV__: process.env.NODE_ENV === 'development',
         __GRAPHQL_URL__: '"http://localhost:8091/graphql"',
-    }
-}
-let config = {
+    },
+};
+const config = {
     target: 'electron-renderer',
     entry: {
-        renderer: [
-            './src/renderer/main.tsx',
-        ],
-        tray: [
-            './src/renderer/tray.tsx',
-        ],
-        about: [
-            './src/renderer/about.tsx',
-        ]
+        renderer: ['./src/renderer/main.tsx'],
+        tray: ['./src/renderer/tray.tsx'],
+        about: ['./src/renderer/about.tsx'],
     },
     output: {
         filename: '[name].js',
     },
     plugins: [
         new Dotenv({
-            path: process.env.ENV_FILE
+            path: process.env.ENV_FILE,
         }),
-        new webpack.DefinePlugin(
-            {
-                __DEV__: process.env.NODE_ENV === 'development',
-                __GRAPHQL_URL__: '"http://localhost:8091/graphql"',
-                __CLIENT__: true,
-                __SSR__: false,
-                __PERSIST_GQL__: false,
-                __FRONTEND_BUILD_DIR__: `'${options.frontendBuildDir}'`,
-                __DLL_BUILD_DIR__: `'${options.dllBuildDir}'`,
-                __DEBUGGING__: false,
-            }
-        ),
         new webpack.DefinePlugin({
-            "__ENV__": JSON.stringify(dotenv.parsed)
+            __DEV__: process.env.NODE_ENV === 'development',
+            __GRAPHQL_URL__: '"http://localhost:8091/graphql"',
+            __CLIENT__: true,
+            __SSR__: false,
+            __PERSIST_GQL__: false,
+            __FRONTEND_BUILD_DIR__: `'${options.frontendBuildDir}'`,
+            __DLL_BUILD_DIR__: `'${options.dllBuildDir}'`,
+            __DEBUGGING__: false,
+        }),
+        new webpack.DefinePlugin({
+            __ENV__: JSON.stringify(dotenv.parsed),
         }),
         new CopyWebpackPlugin({
-            patterns: [{
-                from: 'assets/html/tray-page.html',
-                to: 'tray-page.html',
-            }, {
-                from: 'assets/html/about-page.html',
-                to: 'about-page.html',
-            }, {
-                from: 'assets/html/main-page.html',
-                to: 'main-page.html',
-            },
-
-            ]
+            patterns: [
+                {
+                    from: 'assets/html/tray-page.html',
+                    to: 'tray-page.html',
+                },
+                {
+                    from: 'assets/html/about-page.html',
+                    to: 'about-page.html',
+                },
+                {
+                    from: 'assets/html/main-page.html',
+                    to: 'main-page.html',
+                },
+            ],
         }),
     ],
     // defines: {
@@ -97,32 +88,30 @@ let config = {
     externals: [
         'react',
         'react-redux',
-        "react-redux",
-        "react-apollo",
-        "react-fela",
-        "react-helmet",
-        "react-loadable",
-        "react-redux",
-        "react-resizable",
-        "react-router",
-        "react-router-config",
-        "react-router-dom",
-        "react-router-redux",
-        "react-transition-group"
+        'react-redux',
+        'react-apollo',
+        'react-fela',
+        'react-helmet',
+        'react-loadable',
+        'react-redux',
+        'react-resizable',
+        'react-router',
+        'react-router-config',
+        'react-router-dom',
+        'react-router-redux',
+        'react-transition-group',
     ],
-
-}
+};
 
 if (process.env.NODE_ENV === 'development') {
     const dotEnvPlugin = {
         plugins: [
             new Dotenv({
-                path: process.env.ENV_FILE
-            })
+                path: process.env.ENV_FILE,
+            }),
         ],
-    }
+    };
     // config.builders.web.webpackConfig = merge(config.builders.web.webpackConfig, dotEnvPlugin);
-
 }
 const extraDefines = {
     __SSR__: false,
@@ -133,7 +122,6 @@ const extraDefines = {
 };
 
 if (process.env.NODE_ENV !== 'production') {
-
     // if (!options.ssr) {
     //     console.log('Warning! exposing env variables in UI, only run in development.');
     //     var dotenv = require('dotenv-safe')
