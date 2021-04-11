93e02cfa2ab9c8b5d60728b13bcef2f5c5e6caf5
Veera Marni <marni.srikanth@gmail.com>
2021-04-11 16:37:37 -0400

    add metadata

--- servers/frontend-server/src/index.tsx
+++ servers/frontend-server/src/index.tsx
@@ -1,3 +1,4 @@
+import 'reflect-metadata';
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-explicit-any */
