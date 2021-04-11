a137d07b5781a836b189cb80a7bdd28c5f9c3098
Veera Marni <marni.srikanth@gmail.com>
2021-04-11 12:03:09 -0400

    update yarn

--- servers/frontend-server/.zenrc.js
+++ servers/frontend-server/.zenrc.js
@@ -8,9 +8,6 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
-const pp =  path.resolve(process.cwd(), 'dist');
-
-console.log('---PPP', pp);
 const config = {
     builders: {
         web: {
@@ -36,9 +33,6 @@ const config = {
             waitOn: ['tcp:localhost:8080'],
             enabled: true,
             webpackConfig: {
-                output: {
-                    path: pp,
-                },
                 plugins: [
                     new LodashModuleReplacementPlugin({
                         // Necessary as a workaround for https://github.com/apollographql/react-apollo/issues/1831
@@ -57,9 +51,6 @@ const config = {
         },
         server: {
             entry: './src/backend/app.ts',
-            output: {
-                path: pp,
-            },
             stack: ['server'],
             tsLoaderOptions: {
                 // configFileName: "./tsconfig.json"
@@ -71,8 +62,6 @@ const config = {
             webpackConfig: {
                 output: {
                     filename: 'main.js',
-                    path: pp,
-
                 },
                 plugins: [
                     new CopyWebpackPlugin({
