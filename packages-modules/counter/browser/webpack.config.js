436005c3a0a4e0e47fd59e7350d0cc9a2c17c896
Veera Marni <marni.srikanth@gmail.com>
2021-05-03 11:35:19 -0400

    update webpack

--- packages-modules/counter/browser/webpack.config.js
+++ packages-modules/counter/browser/webpack.config.js
@@ -1,64 +1,67 @@
-var nodeExternals = require('webpack-node-externals');
-var webpack = require('webpack');
-var path = require('path');
+const nodeExternals = require('webpack-node-externals');
+const webpack = require('webpack');
+const path = require('path');
 
-var webpackOpts = {
-  mode: 'development',
-  entry: {
-    index: './src/index.ts',
-    ['index.native']: './src/index.native.ts',
-  },
-  target: 'node',
-  output: {
-    path: path.join(__dirname, 'lib'),
-    filename: '[name].js',
-    libraryTarget: "commonjs2",
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.graphql', '.graphqls', '.gql', '.native.tsx', '.native.ts']
-  },
-  plugins: [
-    new webpack.LoaderOptionsPlugin({
-      options: {
-        test: /\.tsx?$/,
-        ts: {
-          compiler: 'typescript',
-          configFile: 'tsconfig.json'
-        },
-        tslint: {
-          emitErrors: true,
-          failOnHint: true
-        }
-      }
-    })
-  ],
-  devtool: 'source-map',
-  module: {
-    rules: [{
-      test: /\.tsx?$/,
-      loaders: 'ts-loader',
-      options: {
-        compilerOptions: {
-          outDir: path.join()
-        }
-      }
+const webpackOpts = {
+    mode: 'development',
+    entry: {
+        index: './src/index.ts',
+        'index.native': './src/index.native.ts',
     },
-    {
-      test: /\.(gql)$/,
-      exclude: /node_modules/,
-      use: ['graphql-tag/loader']
+    target: 'node',
+    output: {
+        path: path.join(__dirname, 'lib'),
+        filename: '[name].js',
+        libraryTarget: 'commonjs2',
     },
-    {
-      test: /\.graphql?/,
-      exclude: /node_modules/,
-      use: 'raw-loader',
-    }
-    ]
-  },
-  externals: [
-    nodeExternals({ modulesDir: "../../../node_modules" }),
-    nodeExternals()
-  ]
+    resolve: {
+        extensions: ['.ts', '.tsx', '.graphql', '.graphqls', '.gql', '.native.tsx', '.native.ts'],
+    },
+    plugins: [
+        new webpack.LoaderOptionsPlugin({
+            options: {
+                test: /\.tsx?$/,
+                ts: {
+                    compiler: 'typescript',
+                    configFile: 'tsconfig.json',
+                },
+                tslint: {
+                    emitErrors: true,
+                    failOnHint: true,
+                },
+            },
+        }),
+    ],
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loaders: 'ts-loader',
+                options: {
+                    compilerOptions: {
+                        outDir: path.join(),
+                    },
+                },
+            },
+            {
+                test: /\.mjs$/,
+                include: /node_modules/,
+                type: 'javascript/auto',
+            },
+            {
+                test: /\.(gql)$/,
+                exclude: /node_modules/,
+                use: ['graphql-tag/loader'],
+            },
+            {
+                test: /\.graphql?/,
+                exclude: /node_modules/,
+                use: 'raw-loader',
+            },
+        ],
+    },
+    externals: [nodeExternals({ modulesDir: '../../../node_modules' }), nodeExternals()],
 };
 
 module.exports = webpackOpts;
