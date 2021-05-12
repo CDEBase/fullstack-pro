ae6026bc426bebb1870d8118712abb201e6074b7
Veera Marni <marni.srikanth@gmail.com>
2021-05-12 15:25:38 -0400

    remove native code

--- packages-modules/counter/browser/webpack.config.js
+++ packages-modules/counter/browser/webpack.config.js
@@ -6,7 +6,6 @@ const webpackOpts = {
     mode: 'development',
     entry: {
         index: './src/index.ts',
-        'index.native': './src/index.native.ts',
     },
     target: 'node',
     output: {
