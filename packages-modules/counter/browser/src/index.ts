813ebe8912ea22f6007ae9deeb90a1075ea24954
Veera Marni <marni.srikanth@gmail.com>
2021-05-14 18:52:06 -0400

    to make electron packages watch to work

--- packages-modules/counter/browser/src/index.ts
+++ packages-modules/counter/browser/src/index.ts
@@ -2,9 +2,9 @@ import { Feature } from '@common-stack/client-react';
 
 import Common from './common';
 import ApolloCounter from './apollo-server-n-client';
-import ConnectedReactRouter, { Counter } from './connected-react-router';
+import ConnectedReactRouter from './connected-react-router';
 import Fela from './fela';
 import { ElectronTrayModule, ElectronMainModule } from './connected-react-router/index.electron';
 
 export default new Feature(Common, ConnectedReactRouter, ApolloCounter, Fela);
-export { ElectronTrayModule, ElectronMainModule, Counter };
+export { ElectronTrayModule, ElectronMainModule };
--- portable-devices/desktop/tools/utils.js
+++ portable-devices/desktop/tools/utils.js
@@ -1,43 +1,11 @@
-/* eslint-disable no-param-reassign */
-const fs = require('fs');
 const path = require('path');
-const workspaceUtils = require('./workspaces-utils');
+const getSymlinkedNodeModulesForDirectory = require('../../../tools/get-symlinked-modules');
 
 function getWorkspacePackagePaths(root) {
-    console.log('--_ROOT', root)
-    console.log('--GET WORKSPACES', workspaceUtils.getWorkSpacePackages(root))
-    return workspaceUtils.getWorkSpaceDirPaths(root).map((dir) => path.resolve(dir));
+    const symlinkedModules = getSymlinkedNodeModulesForDirectory(root);
+    const symlinkedModulePaths = Object.values(symlinkedModules);
+    return symlinkedModulePaths.map((f) => path.resolve(f, 'lib'));
 }
-
-function isWorkspacePackageFile(filePath, workspacePackagePaths) {
-    filePath = path.resolve(filePath);
-    return workspacePackagePaths.some((packagePath) => filePath.startsWith(packagePath));
-}
-
-function createAliases(workspacePackagePaths, libAlias) {
-    return workspacePackagePaths.reduce((result, packagePath) => {
-        console.log('--packagePath', packagePath);
-        const { name } = require(path.resolve(packagePath, 'package.json'));
-        const useLib = libAlias && fs.existsSync(path.resolve(packagePath, libAlias));
-
-        return {
-            ...result,
-            [name]: path.resolve(useLib ? `${packagePath}/${libAlias}` : packagePath),
-        };
-    }, {});
-}
-
-function overridePlugin(plugin, workspacePackagePaths, overrides) {
-    const override = overrides[plugin.constructor.name];
-    if (override) {
-        return override(plugin, workspacePackagePaths);
-    }
-    return plugin;
-}
-
 module.exports = {
     getWorkspacePackagePaths,
-    isWorkspacePackageFile,
-    createAliases,
-    overridePlugin,
 };
