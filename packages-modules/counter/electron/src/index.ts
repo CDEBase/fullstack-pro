6907e67b067b3723f54cc94727ebdbcdd32b3534
Veera Marni <marni.srikanth@gmail.com>
2021-05-20 09:58:07 -0400

    electron-module

---
+++ packages-modules/counter/electron/src/index.ts
@@ -0,0 +1,4 @@
+import { Feature } from '@common-stack/client-react';
+import { ElectronMainModule } from '@sample-stack/counter-module-browser/lib/connected-react-router/index.electron';
+
+export default new Feature(ElectronMainModule);
