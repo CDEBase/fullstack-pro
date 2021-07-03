b17e546deefa50e36bed1d93f484b13d99617200
Veera Marni <marni.srikanth@gmail.com>
2021-07-03 13:13:55 -0400

    cleanup

--- portable-devices/mobile/src/App.tsx
+++ portable-devices/mobile/src/App.tsx
@@ -11,45 +11,20 @@ import { persistStore, persistReducer } from 'redux-persist';
 import { PersistGate } from 'redux-persist/integration/react';
 // import { RendererProvider } from 'react-fela';
 import { ConnectedRouter } from 'connected-react-router';
-// import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
-// import { NativeRouter } from 'react-router-native';
 import {
   createReduxStore,
   history,
 } from './config/redux-config';
 import { createApolloClient } from './config/apollo-client';
-// import env from './config/public-config';
-// import config from './config';
-// import useColorScheme from './hooks/useColorScheme';
-// import useCachedResources from './hooks/useCachedResources';
-
-// import { MainRoute } from './modules';
 
 const client = createApolloClient();
-
-// let store: any;
-// if ((module as any).hot && (module as any).hot.data && (module as any).hot.data.store) {
-//   // console.log('Restoring Redux store:', JSON.stringify((module as any).hot.data.store.getState()));
-//   store = (module as any).hot.data.store;
-//   // replace the reducers always as we don't have ablity to find
-//   // new reducer added through our `modules`
-//   store.replaceReducer(
-//     persistReducer(persistConfig, storeReducer((module as any).hot.data.history || history)),
-//   );
-// } else {
-//   store = createReduxStore();
-// }
 
 const store = createReduxStore();
 const renderer = createRenderer();
 
 // console.log('---CONFIG--new-', config, env);
 export default function App() {
-  // const isLoadingComplete = useCachedResources();
 
-  // if (!isLoadingComplete) {
-  //   return null;
-  // }
   let persistor = persistStore(store as any);
   return (
     <SafeAreaProvider>
