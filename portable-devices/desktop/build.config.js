d43efc528aef32d1cf2da9ffadb022ac2d60a9af
Veera Marni <marni.srikanth@gmail.com>
2021-07-19 19:27:07 -0400

    update changes

--- portable-devices/desktop/build.config.js
+++ portable-devices/desktop/build.config.js
@@ -3,7 +3,7 @@ const config = {
     __SERVER__: false,
     __CLIENT__: true,
     __SSR__: false,
-    __DEBUGGING__: false,
+    __DEBUGGING__: true,
     __TEST__: false,
     __GRAPHQL_URL__: process.env.API_URL || 'http://localhost:8080/graphql',
     __BACKEND_URL__: process.env.WEBSITE_URL || 'http://localhost:8080',
