e4f1bddb6459ab927b722ba0335f3e244e384a7c
Veera Marni <marni.srikanth@gmail.com>
2021-10-12 09:32:58 -0400

    correct types

--- servers/backend-server/src/connectors/mongo-connector.ts
+++ servers/backend-server/src/connectors/mongo-connector.ts
@@ -1,40 +1,43 @@
-import * as mongoose from 'mongoose';
+import { createConnection, connection, ConnectionOptions, Connection } from 'mongoose';
 import * as _ from 'lodash';
 import { Db } from 'mongodb';
 import { logger } from '@cdm-logger/server';
 import { CdmLogger } from '@cdm-logger/core';
+
 type ILogger = CdmLogger.ILogger;
 
-
-
 export class MongoConnector {
+    private client: Connection;
 
-    private client: mongoose.Connection;
     private db: Db;
-    private opts: mongoose.ConnectionOptions;
+
+    private opts: ConnectionOptions;
+
     private uri: string;
+
     private logger: ILogger;
 
-    constructor(uri: string, opts?: mongoose.ConnectionOptions) {
+    constructor(uri: string, opts?: ConnectionOptions) {
         this.opts = _.defaultsDeep(opts, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
         this.uri = uri;
-        this.logger = logger.child({className: 'MongoConnector'});
+        this.logger = logger.child({ className: 'MongoConnector' });
     }
+
     /**
      * Connect to database
      *
      * @memberof MongoConnector
      */
-    public async connect(): Promise<mongoose.Connection> {
+    public async connect(): Promise<Connection> {
         if (this.client) {
             return this.client;
         }
-        const conn =  mongoose.createConnection(this.uri, this.opts);
+        const conn = createConnection(this.uri, this.opts);
 
-        conn.then(result => {
+        conn.then((result) => {
             this.client = conn;
 
             if ((result as any).connection) {
@@ -46,7 +49,7 @@ export class MongoConnector {
             this.logger.info(' MongoDB has connected successfully.');
 
             this.db.on('disconnected', () => this.logger.warn('Mongoose has disconnected.'));
-            this.db.on('error', err => this.logger.error('MongoDB error.', err));
+            this.db.on('error', (err) => this.logger.error('MongoDB error.', err));
             this.db.on('reconnect', () => this.logger.info('Mongoose has reconnected.'));
         });
         return conn;
@@ -64,6 +67,6 @@ export class MongoConnector {
         if (this.db && (this.db as any).close) {
             await (this.db as any).close();
         }
-        await mongoose.connection.close();
+        await connection.close();
     }
 }
