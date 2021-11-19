d56cd987f9d3cfcd265ab5fcdedc589841287f61
Veera Marni <marni.srikanth@gmail.com>
2021-11-19 11:24:16 -0500

    update native-base and few fixes

--- portable-devices/mobile/webpack.config.js
+++ portable-devices/mobile/webpack.config.js
@@ -1,9 +1,9 @@
 const webpack = require('webpack');
 const path = require('path');
-const { createWebpackConfigAsync } = require('expo-yarn-workspaces/webpack');
+const createExpoWebpackConfigAsync = require('@expo/webpack-config');
 
 module.exports = async function (env, argv) {
-    const config = await createWebpackConfigAsync(env, argv);
+    const config = await createExpoWebpackConfigAsync(env, argv);
 
     config.module.rules.push(
         {
