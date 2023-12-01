14898a5cf5d6c78b8749fde5e18f9b04c43f379a
veera <marni.srikanth@gmail.com>
2023-12-01 09:40:14 -0500

    update

--- tools/deployVersionUpdate.js
+++ tools/deployVersionUpdate.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 // Define file paths
 const JENKINSFILE_PATH = path.join(__dirname, '../Jenkinsfile');
-const CONFIG_FILE_PATH = path.join(__dirname, '../path/to/configFile.yaml');
+const CONFIG_FILE_PATH = path.join(__dirname, '../values-dev.yaml');
 const LERNA_JSON_PATH = path.join(__dirname, '../lerna.json');
 
 function runScript(scriptPath, args) {
@@ -35,9 +35,9 @@ function main() {
     const updateJenkinsfileScript = path.join(__dirname, 'updateJenkinsfile.js');
 
     // Run update scripts with predefined paths
-    runScript(updateLernaScript, [version, LERNA_JSON_PATH]);
-    runScript(updateConfigScript, [version, CONFIG_FILE_PATH]);
-    runScript(updateJenkinsfileScript, [version, JENKINSFILE_PATH]);
+    runScript(updateLernaScript, [LERNA_JSON_PATH, version]);
+    runScript(updateConfigScript, [CONFIG_FILE_PATH, version]);
+    runScript(updateJenkinsfileScript, [JENKINSFILE_PATH, version]);
 
     // Perform Git operations
     gitOperations(version);
