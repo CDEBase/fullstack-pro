d154efdfdce81bbe08bab3eb053d4257d04e2404
Devmax <maxsamuel214@protonmail.com>
2024-01-14 13:42:18 -0800

    Set cache expire with 300s

--- servers/frontend-server/src/backend/middlewares/cache.ts
+++ servers/frontend-server/src/backend/middlewares/cache.ts
@@ -1,8 +1,8 @@
-import { createClient } from 'redis';
+import Redis from 'ioredis';
 import { logger } from '@cdm-logger/server';
 import { config } from '../../config';
 
-const client = createClient({ url: config.REDIS_URL })
+const redis = new Redis(config.REDIS_URL);
 
 client.on('connect', ()=>logger.info('Redis Client connected in Client Server'));
 client.on('error', err => logger.error('Redis Client Error', err));
@@ -12,13 +12,13 @@ export const cacheMiddleware = (req, res, next) => {
   res.setHeader('Cache-Control', `public, max-age=300, s-maxage=3600`)
 
   const key = req.url
-  client.get(key, (err, value) => {
+  redis.get(key, (err, value) => {
     if (err == null && value != null) {
       res.send(value)
     } else {
       res.sendResponse = res.send
       res.send = async (body) => {
-        const reply = await client.set(key, body, 'EX', 100)
+        const reply = await redis.set(key, body, 'EX', 300)
         logger.info(`Redis Client set new value of key - ${key}`, reply)
         res.sendResponse(body)
       }
