61befed899a49da03c98ff927cc8154616776706
veera <marni.srikanth@gmail.com>
2024-02-02 20:36:06 -0500

    update

--- portable-devices/mobile/src/config/redux-config.ts
+++ portable-devices/mobile/src/config/redux-config.ts
@@ -19,6 +19,7 @@ export const epicMiddlewareFunc = (apolloClient, services, container) =>
             logger,
             config: {
                 loadRoot: true,
+                isMobile: true,
             },
         },
     });
