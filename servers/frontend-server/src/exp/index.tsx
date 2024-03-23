f5feec40ab41bbb1ee670cbff04b060f18c7b087
Devmax <maxsamuel214@protonmail.com>
2024-03-23 13:52:51 -0700

    Add codegen

--- servers/frontend-server/src/exp/index.tsx
+++ servers/frontend-server/src/exp/index.tsx
@@ -1,6 +1,8 @@
 import { Link, Outlet } from '@remix-run/react';
 import type { MetaFunction } from "@remix-run/node";
 
+import OuterModule from '../routes/outer.codegen';
+
 export const meta: MetaFunction = () => {
   return [
     { title: "Fullstack Pro" },
@@ -9,6 +11,7 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
+  // return OuterModule();
   return (
     <div style={{ 
       fontFamily: "system-ui, sans-serif", 
@@ -21,12 +24,15 @@ export default function Index() {
           <li><Link to={'/demo'}>Demo</Link></li>
           <li><Link to={'/demo/counter'}>Demo/Counter</Link></li>
           <li><Link to={'/demo/counter/10'}>Demo/Counter with param 10</Link></li>
-          <li><Link to={'/outer'}>Outer modules in node_modules</Link></li>
+          <li><Link to={'/codegen'}>Outer modules with codegen</Link></li>
         </ul>
       </aside>
       <section style={{ flexGrow: 2 }}>
         <h1>Welcome to Remix</h1>
         <Outlet />
+        <div id="outer">
+          {OuterModule()}
+        </div>
       </section>
     </div>
   );
--- servers/frontend-server/vite.config.ts
+++ servers/frontend-server/vite.config.ts
@@ -1,16 +1,22 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import codegen from 'vite-plugin-codegen';
+// import babel from 'vite-plugin-babel';
 import { generateRemixRoutes } from "./src/routes";
 
 export default defineConfig({
+  // assetsInclude: ['**/*.codegen'],
   plugins: [
+    // babel(),
+    tsconfigPaths({ ignoreConfigErrors: true }),
+    (codegen as any).default(),
     remix({
       appDirectory: "src",
-      routes: async (defineRoutes) => defineRoutes((route) => {
-        generateRemixRoutes(route);
-      }),
+      routes: async (defineRoutes) => 
+        defineRoutes((route) => {
+          generateRemixRoutes(route);
+        }),
     }),
-    tsconfigPaths({ ignoreConfigErrors: true }),
   ],
 });
