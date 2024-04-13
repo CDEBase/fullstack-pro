f486d95aeba70e601a5c806f7fcacb947263399a
veera <marni.srikanth@gmail.com>
2024-04-13 19:20:14 -0400

    rollup changes

--- rollup.config.base.mjs
+++ rollup.config.base.mjs
@@ -1,4 +1,4 @@
-// rollup.config.base.js
+i// rollup.config.base.js
 import graphql from '@rollup/plugin-graphql';
 import image from '@rollup/plugin-image';
 import typescript from '@rollup/plugin-typescript';
@@ -7,12 +7,10 @@ import { parse } from '@babel/parser';
 import traverse from '@babel/traverse';
 import generate from '@babel/generator';
 import { copy } from '@web/rollup-plugin-copy';
-import modifyLibFilesPlugin from './rollupPluginModifyLibFiles.mjs';
-import generateJsonFromObject from './rollupPluginGenerateJson.mjs';
+import modifyLibFilesPlugin from './tools/rollup/rollupPluginModifyLibFiles.mjs';
+import generateJsonFromObject from './tools/rollup/rollupPluginGenerateJson.mjs';
 // Define any additional plugins specific to this bundle
-const additionalPlugins = [
-    copy({ patterns: '**/cdm-locales/**/*', rootDir: './src' })
-];
+const additionalPlugins = [copy({ patterns: '**/cdm-locales/**/*', rootDir: './src' })];
 
 // Optional: Define commonly used custom plugins/functions if applicable
 function appendJsExtensionForMonacoEditor() {
@@ -36,7 +34,7 @@ const addJsExtensionToImportsPlugin = () => {
         name: 'add-js-extension-to-imports',
         transform(code, id) {
             // Only apply the transformation to .js files
-            if (!/\.tsx?$/.test(id)) return null; 
+            if (!/\.tsx?$/.test(id)) return null;
 
             const ast = parse(code, {
                 sourceType: 'module',
@@ -102,6 +100,100 @@ const baseConfig = {
 // Function to create a configuration by extending the base
 function createRollupConfig(overrides) {
     return deepMergeConfigs(baseConfig, overrides);
+}
+
+function watch(watchOptions, buildMode = '') {
+    const filename = path.basename(watchOptions.input);
+    message(
+        'note',
+        `${dt()} Rollup: Watcher Starting - watching for changes starting with: "${filename}" buildMode="${buildMode}"...`,
+        'WATCH  ',
+        true,
+    );
+    const watcher = rollup.watch(watchOptions);
+
+    watcher.on('event', (event) => {
+        // event.code can be one of:
+        //   START        — the watcher is (re)starting
+        //   BUNDLE_START — building an individual bundle
+        //                  * event.input will be the input options object if present
+        //                  * event.output contains an array of the "file" or
+        //                    "dir" option values of the generated outputs
+        //   BUNDLE_END   — finished building a bundle
+        //                  * event.input will be the input options object if present
+        //                  * event.output contains an array of the "file" or
+        //                    "dir" option values of the generated outputs
+        //                  * event.duration is the build duration in milliseconds
+        //                  * event.result contains the bundle object that can be
+        //                    used to generate additional outputs by calling
+        //                    bundle.generate or bundle.write. This is especially
+        //                    important when the watch.skipWrite option is used.
+        //                  You should call "event.result.close()" once you are done
+        //                  generating outputs, or if you do not generate outputs.
+        //                  This will allow plugins to clean up resources via the
+        //                  "closeBundle" hook.
+        //   END          — finished building all bundles
+        //   ERROR        — encountered an error while bundling
+        //                  * event.error contains the error that was thrown
+        //                  * event.result is null for build errors and contains the
+        //                    bundle object for output generation errors. As with
+        //                    "BUNDLE_END", you should call "event.result.close()" if
+        //                    present once you are done.
+        // If you return a Promise from your event handler, Rollup will wait until the
+        // Promise is resolved before continuing.
+        // console.log(`rollup: ${event.code}`)
+        if (event.code === 'BUNDLE_END') {
+            const outputFiles = event.output.map((o) => path.basename(o)).join(', .../');
+            const msg = `${dt()} Rollup: wrote bundle${event.output.length > 1 ? 's' : ''}: ".../${outputFiles}"`;
+            if (NOTIFY) {
+                notifier.notify({
+                    title: 'React Component Build',
+                    message: msg,
+                });
+            }
+            // messenger: success, warn, critical, note, log
+            message('success', msg, 'SUCCESS', true);
+        } else if (event.code === 'ERROR') {
+            message('critical', `!!!!!!!!!!!!!!!\nRollup ${event.error}\n!!!!!!!!!!!!!!!\n`, 'ERROR', true);
+            if (NOTIFY) {
+                notifier.notify({
+                    title: 'NotePlan Plugins Build',
+                    message: `An error occurred during build process.\nSee console for more information`,
+                });
+            }
+        }
+    });
+
+    // This will make sure that bundles are properly closed after each run
+    watcher.on('event', ({ result }) => {
+        if (result) {
+            result.close();
+        }
+    });
+
+    // Additionally, you can hook into the following. Again, return a Promise to
+    // make Rollup wait at that stage:
+    watcher.on('change', (id /* , { event } */) => {
+        const filename = path.basename(id);
+        message('info', `${dt()} Rollup: file: "${filename}" changed`, 'CHANGE', true);
+        /* a file was modified */
+    });
+    watcher.on('restart', () => {
+        // console.log(`rollup: restarting`)
+        /* a new run was triggered (usually a watched file change) */
+    });
+    watcher.on('close', () => {
+        console.log(`rollup: closing`);
+        /* the watcher was closed, see below */
+    });
+    process.on('SIGINT', async function () {
+        console.log('\n\n');
+        console.log(colors.yellow('Quitting...\n'));
+        if (watcher) {
+            await watcher.close();
+        }
+        process.exit();
+    });
 }
 
 export { createRollupConfig, baseConfig };
---
+++ tools/rollup/rollupPluginModifyLibFiles.mjs
@@ -0,0 +1,193 @@
+import fs from 'fs';
+import path from 'path';
+import { parse } from '@babel/parser';
+import traverse from '@babel/traverse';
+import generate from '@babel/generator';
+import * as t from '@babel/types';
+import { promisify } from 'util';
+import glob from 'glob';
+import { createFilter } from '@rollup/pluginutils';
+const globPromise = promisify(glob.glob); // Make sure to call .glob here
+
+function findPackageJson(directory) {
+    let currentDir = directory;
+    while (currentDir && currentDir !== path.parse(currentDir).root) {
+        const packageJsonPath = path.join(currentDir, 'package.json');
+        if (fs.existsSync(packageJsonPath)) {
+            return packageJsonPath;
+        }
+        currentDir = path.dirname(currentDir);
+    }
+    throw new Error(`No package.json found in path ${directory}`);
+}
+
+export default function modifyLibFilesPlugin(options = {}) {
+    // Create a filter to only include the desired files
+    const filter = createFilter(options.include, options.exclude);
+    const dist = options.outputDir || './lib'; // Default output directory
+    const pattern = '**/**/compute.js'; // Pattern to match files
+
+    return {
+        name: 'modify-lib-files',
+
+        async writeBundle(outputOptions) {
+            const currentWorkingDir = process.cwd();
+            // Assuming you want to modify specific files, list them here
+            const filesToModify = await globPromise(path.join(dist, pattern), { absolute: true }); // Ensure paths are absolute
+            filesToModify.forEach((filePath) => {
+                if (!filter(filePath)) return; // Skip files that do not match the filter
+
+                // Read the file content
+                const code = fs.readFileSync(filePath, 'utf8');
+                // Parse the code to an AST
+                const ast = parse(code, {
+                    sourceType: 'module',
+                    plugins: ['js', 'dynamicImport'], // Adjust plugins as necessary
+                });
+
+                // Traverse and modify the AST as needed
+                let modified = false;
+                traverse.default(ast, {
+                    enter(astroPath) {
+                        // Adjust this part to target the specific objects and properties in your AST
+                        if (
+                            astroPath.isObjectProperty() &&
+                            (astroPath.node.key.name === 'component' || astroPath.node.key.name === 'dialog')
+                        ) {
+                            const importDeclaration = astroPath.node.value;
+                            const propName = astroPath.node.key.name;
+
+                            if (
+                                importDeclaration.type === 'ArrowFunctionExpression' &&
+                                importDeclaration.body.type === 'CallExpression' &&
+                                importDeclaration.body.callee.type === 'Import'
+                            ) {
+                                const importArg = importDeclaration.body.arguments[0];
+
+                                // Ensure we're dealing with a string literal import path
+                                if (importArg.type === 'StringLiteral') {
+                                    let importPath = importArg.value;
+                                    let hasLoader = false;
+                                    let hasAction = false;
+                                    const fullPath = path.resolve(path.dirname(filePath), importPath);
+                                    if(astroPath.node.key.name === 'component' && fs.existsSync(fullPath)) {
+                                        const importedFileCode = fs.readFileSync(fullPath, 'utf8');
+                                        const importedAst = parse(importedFileCode, {
+                                            sourceType: 'module',
+                                            plugins: ['typescript', 'jsx'],
+                                        });
+
+        
+                                        traverse.default(importedAst, {
+                                            ExportNamedDeclaration(namedPath) {
+                                                namedPath.node.specifiers.forEach(specifier => {
+                                                    if (specifier.exported.name === 'loader') {
+                                                        hasLoader = true;
+                                                    }
+                                                    if (specifier.exported.name === 'action') {
+                                                        hasAction = true;
+                                                    }
+                                                });
+                                            }
+                                        });
+                                    }
+                                    const packageJsonPath = findPackageJson(
+                                        path.dirname(path.resolve(process.cwd(), dist)),
+                                    );
+                                    const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
+                                    const relativePath = path.relative(path.dirname(packageJsonPath), fullPath);
+                                    const normalizedImportPath = relativePath.replace(/\\/g, '/'); // Normalize path for Windows
+
+                                    const newImportPath = `${packageJson.name}/${normalizedImportPath}`;
+
+                                    // Create a new `file` property
+                                    const fileProperty = t.objectProperty(
+                                        t.identifier(`${propName}Path`),
+                                        t.stringLiteral(newImportPath),
+                                    );
+
+                                    // Get the parent object expression to add the new property
+                                    const parentObject = astroPath.findParent((p) => p.isObjectExpression());
+                                    if (parentObject) {
+                                        parentObject.node.properties.push(fileProperty);
+                                        if (hasLoader) {
+                                            parentObject.node.properties.push(t.objectProperty(t.identifier('loader'), t.booleanLiteral(true)));
+                                        }
+                                        if (hasAction) {
+                                            parentObject.node.properties.push(t.objectProperty(t.identifier('action'), t.booleanLiteral(true)));
+                                        }
+                                        modified = true; // Mark as modified
+                                    }
+                                }
+                            }
+                        } else if (
+                            astroPath.node.key &&
+                            astroPath.node.key.name === 'wrappers' &&
+                            astroPath.node.value.type === 'ArrayExpression'
+                        ) {
+                            const parentObjectExpression = astroPath.findParent((p) => p.isObjectExpression());
+                            const wrapperPaths = [];
+                            astroPath.node.value.elements.forEach((element, index) => {
+                                if (element.type === 'CallExpression' && element.callee.type === 'Import') {
+                                    const importArg = element.arguments[0];
+                                    if (importArg && importArg.type === 'StringLiteral') {
+                                        const importPath = importArg.value;
+                                        const fullPath = path.resolve(path.dirname(filePath), importPath);
+                                        const packageJsonPath = findPackageJson(
+                                            path.dirname(path.resolve(process.cwd(), dist)),
+                                        );
+                                        const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
+                                        const relativePath = path.relative(path.dirname(packageJsonPath), fullPath);
+                                        const normalizedImportPath = `${packageJson.name}/${relativePath.replace(
+                                            /\\/g,
+                                            '/',
+                                        )}`;
+                                        // Modify the import path directly
+                                        element.arguments[0] = t.stringLiteral(normalizedImportPath);
+
+                                        wrapperPaths.push(normalizedImportPath);
+
+                                        if (wrapperPaths.length > 0) {
+                                            // Construct an array expression for the wrapper paths
+                                            const wrapperPathsArrayExpression = t.arrayExpression(
+                                                wrapperPaths.map((path) => t.stringLiteral(path)),
+                                            );
+                                            // Create an object property AST node for `wrapperPaths`
+                                            const wrapperPathsProperty = t.objectProperty(
+                                                t.identifier('wrapperPaths'), // Property key
+                                                wrapperPathsArrayExpression, // Property value
+                                            );
+                                            // Ensure the parent object expression exists and has properties
+                                            if (
+                                                parentObjectExpression &&
+                                                parentObjectExpression.node &&
+                                                parentObjectExpression.node.properties
+                                            ) {
+                                                // Push the new property into the parent object's properties array
+                                                parentObjectExpression.node.properties.push(wrapperPathsProperty);
+                                                modified = true; // Mark as modified
+                                            }
+                                        }
+                                    }
+                                }
+                            });
+                        }
+                    },
+                });
+                // If AST was modified, regenerate code
+                if (modified) {
+                    const output = generate.default(
+                        ast,
+                        {
+                            /* options */
+                        },
+                        code,
+                    );
+                    fs.writeFileSync(filePath, output.code); // This line actually writes the changes
+                }
+                // This plugin doesn't modify the code, so return null
+                return null;
+            });
+        },
+    };
+}
