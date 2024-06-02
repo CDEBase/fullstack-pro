4b79ff942b4bea412d817340a3b5c24e6af0d859
veera <marni.srikanth@gmail.com>
2024-06-02 08:45:05 -0400

    update

--- servers/frontend-server/src/app/ErrorBoundary.tsx
+++ servers/frontend-server/src/app/ErrorBoundary.tsx
@@ -1,28 +1,19 @@
 import * as React from 'react';
 import { Error500 } from './500';
-import {
-  isRouteErrorResponse,
-  useRouteError,
-} from "@remix-run/react";
+import { isRouteErrorResponse, useRouteError } from '@remix-run/react';
 
 export function ErrorBoundary() {
-  const error = useRouteError();
+    const error = useRouteError();
 
-  React.useEffect(() => {
-    console.trace(error);
-  }, [error]);
+    React.useEffect(() => {
+        console.trace(error);
+    }, [error]);
 
-  if (isRouteErrorResponse(error)) {
-    return (
-      <Error500 title={error.statusText} status={error.status} data={error.data} />
-    );
-  } else if (error instanceof Error) {
-    return (
-      <Error500 title={error.message} status="500" data={error.stack} />
-    );
-  } else {
-    return (
-      <Error500 title="Unknown Error" status="500" data={error} />
-    );
-  }
+    if (isRouteErrorResponse(error)) {
+        return <Error500 title={error.statusText} status={error.status} data={error.data} />;
+    } else if (error instanceof Error) {
+        return <Error500 title={error.message} status="500" data={error.stack} />;
+    } else {
+        return <Error500 title="Unknown Error" status="500" data={error} />;
+    }
 }
